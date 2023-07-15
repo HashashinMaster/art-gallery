616076498ccbdce008e2e7bb36ec3d429cebf0f3
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-15 19:18:12 +0200

    handle paint context

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -2,41 +2,53 @@ import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 import { useFrame } from "@react-three/fiber";
+import { useTexture } from "@react-three/drei";
+import { useContext } from "react";
+import { PaintContext } from "../App";
 
 type GLTFResult = GLTF & {
   nodes: {
-    Plane_1: THREE.Mesh;
-    Plane_2: THREE.Mesh;
+    frame: THREE.Mesh;
+    paint: THREE.Mesh;
   };
   materials: {
-    ["paint material"]: THREE.MeshStandardMaterial;
-    ["wood  material"]: THREE.MeshStandardMaterial;
+    ["Material.002"]: THREE.MeshStandardMaterial;
   };
 };
 
 export default function Frame(props: JSX.IntrinsicElements["group"]) {
   const { nodes, materials } = useGLTF(
-    "/assets/models/frames/wood_frame.glb"
+    "/assets/models/frames/box-frame.glb"
   ) as GLTFResult;
+  const paint = useContext(PaintContext);
+  let uploadedPaint = useTexture(
+    paint?.paint! ? paint.paint : "/assets/textures/Mona_Lisa.jpg"
+  );
+
   const groupRef = useRef<THREE.Group>(null!);
   useFrame((state, delta) => (groupRef.current.rotation.z += delta));
   return (
     <group
-      ref={groupRef}
       {...props}
-      dispose={null}
       rotation={[Math.PI / 2, 0, 0]}
+      ref={groupRef}
+      dispose={null}
     >
       <mesh
-        geometry={nodes.Plane_1.geometry}
-        material={materials["paint material"]}
+        geometry={nodes.frame.geometry}
+        material={materials["Material.002"]}
+        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
       />
       <mesh
-        geometry={nodes.Plane_2.geometry}
-        material={materials["wood  material"]}
-      />
+        geometry={nodes.paint.geometry}
+        position={[-0.08, 0, 0]}
+        rotation={[(Math.PI * 2) / 2, 0, -Math.PI / 2]}
+        scale={[1.02, 1, 1.03]}
+      >
+        <meshStandardMaterial attach={"material"} map={uploadedPaint} />
+      </mesh>
     </group>
   );
 }
 
-useGLTF.preload("/assets/models/frames/wood_frame.glb");
+useGLTF.preload("/assets/models/frames/box-frame.glb");
