e296e6743a87141edd1fb46248a6aabf08cfcf94
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-24 23:33:58 +0200

    add proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import { join } from "path";
+dotenv.config({ path: join(__dirname, "..", ".env") });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.HOST,
+      },
+    },
+  },
+});
