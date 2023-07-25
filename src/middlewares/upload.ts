0772c522f58d1056d9b1b394edf35447652ffa6b
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-25 21:17:53 +0200

    finish upload ai voice middleware

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -6,7 +6,6 @@ import axios from "axios";
 import { createWriteStream } from "fs";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log("first");
     cb(null, join(__dirname, "..", "..", "uploads", "images"));
   },
   filename: function (req, file, cb) {
@@ -26,13 +25,13 @@ export const uploadAiVoice = async (
     const { data } = await axios.post(
       `https://api.elevenlabs.io/v1/text-to-speech/${req.body.aiVoice}`,
       {
-        text: "hello im just testing sdskoas",
+        text: req.body.description,
       },
       {
         headers: {
           accept: "audio/mpeg", // Set the expected response type to audio/mpeg.
           "content-type": "application/json", // Set the content type to application/json.
-          "xi-api-key": `${process.env.ELEVENLABS_API}`, // Set the API key in the headers.
+          "xi-api-key": `${process.env.ELEVENLABS_API_KEY}`, // Set the API key in the headers.
         },
         responseType: "stream",
       }
@@ -46,7 +45,7 @@ export const uploadAiVoice = async (
     req.body.aiVoice = voiceName;
     next();
   } catch (error: any) {
-    console.log(error.message);
+    console.log("ai voice error: ", error.message);
     res.sendStatus(500);
   }
 };
