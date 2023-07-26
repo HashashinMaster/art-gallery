9d001403c7fbdc0956f515ce7789d72a8046a2be
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-26 19:01:08 +0200

    add light and canvas components

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,3 +1,13 @@
+import Gallery from "../components/Gallery";
+import { Canvas } from "@react-three/fiber";
 export default function App() {
-  return <div>home</div>;
+  return (
+    <div className="h-screen">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Gallery />
+      </Canvas>
+    </div>
+  );
 }
