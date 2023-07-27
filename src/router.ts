09a2bf6821f2667f06cce64b499bba4919bd528f
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-27 20:43:15 +0200

    setup paints router

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { store } from "./controllers/paint";
+import PaintController from "./controllers/paint";
 import { uploadAiVoice, uploadPaint } from "./middlewares/upload";
 
 const router = Router();
 
-router.route("/store").post(uploadPaint, uploadAiVoice, store);
-
+router.route("/store").post(uploadPaint, uploadAiVoice, PaintController.store);
+router.route("/paints").get(PaintController.all);
 export default router;
