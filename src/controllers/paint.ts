f211c27c106330b724d1f0db7c6bf0b233fc9549
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-27 20:25:58 +0200

    from functional controller to class controller

--- src/controllers/paint.ts
+++ src/controllers/paint.ts
@@ -1,19 +1,45 @@
 import { Request, Response } from "express";
 import Paint from "../models/paint";
-export async function store(req: Request, res: Response) {
-  const paintName = req.file?.filename;
-  const paintDocument = {
-    paint: paintName,
-    frame: req.body.frame,
-    position: req.body.position,
-    name: req.body.name,
-    description: req.body.description,
-    aiVoice: req.body.aiVoice,
-  };
-  await Paint.create(paintDocument);
-  res.send({
-    success: true,
-    message: "paint created successfully",
-  });
-  console.log(req.body);
+
+export default class PaintController {
+  static async store(req: Request, res: Response) {
+    const paintName = req.file?.filename;
+    const paintDocument = {
+      paint: paintName,
+      frame: req.body.frame,
+      position: req.body.position,
+      name: req.body.name,
+      description: req.body.description,
+      aiVoice: req.body.aiVoice,
+    };
+    try {
+      await Paint.create(paintDocument);
+      return res.send({
+        success: true,
+        message: "paint created successfully",
+      });
+    } catch (error: any) {
+      console.log(error.message);
+      return res.status(500).send({
+        success: false,
+        message: "something is wrong! Try later.",
+      });
+    }
+  }
+
+  static async all(req: Request, res: Response) {
+    try {
+      const paints = await Paint.find();
+      return res.send({
+        success: true,
+        data: paints,
+      });
+    } catch (error: any) {
+      console.log(error.message);
+      return res.status(500).send({
+        success: false,
+        message: "something is wrong! Try later.",
+      });
+    }
+  }
 }
