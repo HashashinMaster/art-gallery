b182436ac40add2326509a04ae642723191c6507
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-28 22:42:09 +0200

    setup states for Slider Context

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -2,15 +2,33 @@ import Gallery from "./components/Gallery";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loading from "../../components/Loading";
+import SliderArrows from "./components/SliderArrows";
+import { createContext, useState } from "react";
+export const SliderContext = createContext<SliderContext | null>(null);
 export default function App() {
+  const [index, setIndex] = useState(0);
+  const [lastIndex, setLastIndex] = useState(false);
+  const [startSliding, setStartSliding] = useState(false);
   return (
     <div className="h-screen">
       <Suspense fallback={<Loading />}>
-        <Canvas>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Gallery />
-        </Canvas>
+        <SliderContext.Provider
+          value={{
+            lastIndex,
+            setLastIndex,
+            index,
+            setIndex,
+            startSliding,
+            setStartSliding,
+          }}
+        >
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Gallery />
+          </Canvas>
+          <SliderArrows />
+        </SliderContext.Provider>
       </Suspense>
     </div>
   );
