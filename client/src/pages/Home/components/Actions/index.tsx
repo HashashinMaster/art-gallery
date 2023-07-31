334af8111f56a0f489de9c3de7f0a99b6e098b66
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-07-31 21:01:11 +0200

    add button

--- client/src/pages/Home/components/Actions/index.tsx
+++ client/src/pages/Home/components/Actions/index.tsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
 import Play from "./components/Play";
 import SliderArrows from "./components/SliderArrows";
-
+import { GiWoodFrame } from "react-icons/gi";
 export default function Actions() {
   return (
     <div className="flex inset-0 fixed  w-full h-full items-center justify-between p-4">
       <SliderArrows />
       <Play />
+      <div className="absolute top-5 right-5">
+        <Link to={"/add"}>
+          <button className="btn btn-neutral rounded-full">
+            <GiWoodFrame size={"1rem"} />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
