9e6c2e878fa78e4d7347d6cbc87e9908a856a76a
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-08-01 22:30:20 +0200

    change production directory

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,9 @@ const app = express();
 
 //Production setup
 if (process.env.MOD === "PROD") {
-  app.use(express.static(path.resolve(__dirname, "client", "dist")));
+  app.use(express.static(path.resolve(__dirname, "public")));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
   });
 }
 app.use(express.json());
